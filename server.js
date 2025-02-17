46a3e44ab9187d813c7096d6cf0907ce8c12f920
tom914z <168499260+tom914z@users.noreply.github.com>
2025-02-17 11:56:05 -0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+app.use(express.json());
+
+// MySQL connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'fitness_tracker'
+});
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('MySQL connected...');
+});
+
+// Add workout
+app.post('/api/workouts', (req, res) => {
+    const workout = req.body;
+    const sql = 'INSERT INTO workouts (exercise, category, duration, date) VALUES (?, ?, ?, ?)';
+    db.query(sql, [workout.exercise, workout.category, workout.duration, workout.date], (err, result) => {
+        if (err) throw err;
+        res.send('Workout added');
+    });
+});
+
+// Fetch workouts
+app.get('/api/workouts', (req, res) => {
+    db.query('SELECT * FROM workouts', (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
